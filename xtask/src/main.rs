21b16a03202b2cf1fd10a3cde4598776a6909d46
Andrea Venuta <venutawebdesign@gmail.com>
2023-05-20 13:08:42 +0200

    Param mod dist script

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -31,6 +31,7 @@ fn main() -> Result<()> {
     let task = env::args().nth(1);
     match task.as_deref() {
         Some("dist") => dist()?,
+        Some("dist-parammod") => dist_parammod()?,
         Some("inject") => inject(env::args().skip(1).map(String::from))?,
         Some("run") => run()?,
         Some("run-param-tinkerer") => run_param_tinkerer()?,
@@ -50,7 +51,15 @@ fn dist() -> Result<()> {
     let status = Command::new(&cargo)
         .current_dir(project_root())
         .env("CARGO_XTASK_DIST", "true")
-        .args(["build", "--locked", "--release", "--package", "darksoulsiii-practice-tool"])
+        .args([
+            "build",
+            "--locked",
+            "--release",
+            "--package",
+            "darksoulsiii-practice-tool",
+            "--package",
+            "no-logo",
+        ])
         .status()
         .map_err(|e| format!("cargo: {}", e))?;
 
@@ -106,6 +115,60 @@ fn dist() -> Result<()> {
     add_zip(project_root().join("target/release/dinput8nologo.dll"), "dinput8.dll")?;
     add_zip(project_root().join("lib/data/RELEASE-README.txt"), "README.txt")?;
     add_zip(project_root().join("jdsd_dsiii_practice_tool.toml"), "jdsd_dsiii_practice_tool.toml")?;
+
+    Ok(())
+}
+
+fn dist_parammod() -> Result<()> {
+    let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
+
+    let status = Command::new(&cargo)
+        .current_dir(project_root())
+        .env("CARGO_XTASK_DIST", "true")
+        .args([
+            "build",
+            "--locked",
+            "--release",
+            "--package",
+            "param-mod",
+            "--package",
+            "param-tinkerer",
+        ])
+        .status()
+        .map_err(|e| format!("cargo: {}", e))?;
+
+    if !status.success() {
+        return Err("cargo build failed".into());
+    }
+
+    std::fs::remove_dir_all(dist_dir()).ok();
+    std::fs::create_dir_all(dist_dir())?;
+
+    let mut zip = ZipWriter::new(File::create(dist_dir().join("jdsd_dsiii_param_tinkerer.zip"))?);
+    let file_options = FileOptions::default().compression_method(CompressionMethod::Deflated);
+
+    let mut buf: Vec<u8> = Vec::new();
+
+    let mut add_zip = |src: PathBuf, dst: &str| -> Result<()> {
+        File::open(src)
+            .map_err(|e| format!("{}: Couldn't open file: {}", dst, e))?
+            .read_to_end(&mut buf)
+            .map_err(|e| format!("{}: Couldn't read file: {}", dst, e))?;
+        zip.start_file(dst, file_options)
+            .map_err(|e| format!("{}: Couldn't start zip file: {}", dst, e))?;
+        zip.write_all(&buf).map_err(|e| format!("{}: Couldn't write zip: {}", dst, e))?;
+        buf.clear();
+        Ok(())
+    };
+
+    add_zip(project_root().join("target/release/param-tinkerer.exe"), "param-tinkerer.exe")?;
+    add_zip(
+        project_root().join("target/release/jdsd_dsiii_param_tinkerer.dll"),
+        "param-tinkerer.dll",
+    )?;
+    add_zip(project_root().join("target/release/dinput8parammod.dll"), "dinput8.dll")?;
+    add_zip(project_root().join("lib/data/PARAM-TINKERER.txt"), "README.txt")?;
+    add_zip(project_root().join("lib/param-mod/param-mod.toml"), "param-mod.toml")?;
 
     Ok(())
 }
