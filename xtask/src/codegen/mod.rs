e883ef135d7bb947e5bbb382faf85b16da975fe6
Andrea Venuta <venutawebdesign@gmail.com>
2024-03-07 22:07:28 +0100

    Codegen param names

--- xtask/src/codegen/mod.rs
+++ xtask/src/codegen/mod.rs
@@ -6,6 +6,7 @@ use std::io::{BufRead, BufReader, Write};
 use std::process::Command;
 
 use anyhow::{bail, Context};
+use practice_tool_tasks::params::{checkout_paramdex, codegen_param_names};
 use regex::Regex;
 
 use crate::{project_root, Result};
@@ -15,7 +16,7 @@ pub(crate) mod aob_scans;
 pub(crate) fn codegen() -> Result<()> {
     checkout_paramdex()?;
     run_python_script()?;
-    codegen_param_names()?;
+    codegen_param_names("target/Paramdex/DS3/Names", "lib/libds3/src/params/param_names.json")?;
     aob_scans::codegen_base_addresses();
 
     Ok(())
@@ -40,89 +41,6 @@ fn run_python_script() -> Result<()> {
 
     File::create(project_root().join("lib/libds3/src/params/param_data.rs"))?
         .write_all(&cmd.stdout)?;
-
-    Ok(())
-}
-
-fn codegen_param_names() -> Result<()> {
-    let mut data: BTreeMap<String, BTreeMap<usize, String>> = BTreeMap::new();
-
-    let files_with_content = project_root()
-        .join("target/Paramdex/DS3/Names")
-        .read_dir()?
-        .flat_map(|entry| {
-            entry.map(|entry| entry.path()).map(|path| {
-                if path.is_file() && Some("txt") == path.extension().and_then(OsStr::to_str) {
-                    Some(path)
-                } else {
-                    None
-                }
-            })
-        })
-        .flatten();
-
-    let r = Regex::new(r"^(\d+)\s+(.+)").unwrap();
-
-    for path in files_with_content {
-        let stem = path.file_stem().unwrap().to_string_lossy().to_string();
-
-        let data_contents: BTreeMap<_, _> = BufReader::new(File::open(path)?)
-            .lines()
-            .filter_map(|line| {
-                let line = line.ok()?;
-                let cap = r.captures(&line)?;
-
-                let id: usize = cap[1].parse().ok()?;
-                let name: String = cap[2].to_string();
-                Some((id, name))
-            })
-            .collect();
-
-        data.insert(stem, data_contents);
-    }
-
-    serde_json::to_writer(
-        File::create(project_root().join("lib/libds3/src/params/param_names.json"))?,
-        &data,
-    )?;
-
-    Ok(())
-}
-
-fn checkout_paramdex() -> Result<()> {
-    let git = env::var("GIT").unwrap_or_else(|_| "git".to_string());
-
-    if project_root().join("target/Paramdex").exists() {
-        let status = Command::new(&git)
-            .current_dir(project_root().join("target/Paramdex"))
-            .args(["fetch"])
-            .status()
-            .context("git")?;
-
-        if !status.success() {
-            bail!("git fetch failed");
-        }
-
-        let status = Command::new(&git)
-            .current_dir(project_root().join("target/Paramdex"))
-            .args(["pull"])
-            .status()
-            .context("git")?;
-
-        if !status.success() {
-            bail!("git pull failed");
-        }
-    } else {
-        let status = Command::new(&git)
-            .current_dir(project_root().join("target"))
-            .args(["clone", "https://github.com/soulsmods/Paramdex.git"])
-            .status()
-            .context("git")?;
-
-        if !status.success() {
-            bail!("git clone failed");
-        }
-    }
 
     Ok(())
 }
